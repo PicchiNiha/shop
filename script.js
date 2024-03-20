09a70edae0a803c1d639021cf591108ec8664820
PicchiNiha <164085472+PicchiNiha@users.noreply.github.com>
2024-03-20 11:49:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     filterableCards.innerHTML = "";
 
     if (targetName === "all") {
-      fetch(`nihascraftalley.vercel.app/images/all`)
+      fetch(`/images/all`)
         .then((response) => {
           if (!response.ok) {
             throw new Error("Network response was not ok");
